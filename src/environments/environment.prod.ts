ca003c3abc0af6b4a7471a0c6997918b58a27b27
Bruce Carvalho <brucepcarvalho@gmail.com>
2022-11-29 14:21:56 -0400

    fix budget limits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,7 @@
+import {firebase} from './firebase.env';
+
 export const environment = {
-  firebase: {
-    projectId: 'welcomin-ic',
-    appId: '1:479758060518:web:a2f49e5414ef9ef464a427',
-    storageBucket: 'welcomin-ic.appspot.com',
-    apiKey: 'AIzaSyAU-xMb2YE8ysBCnMRQQRRoqZDwYA8dLso',
-    authDomain: 'welcomin-ic.firebaseapp.com',
-    messagingSenderId: '479758060518',
-    measurementId: 'G-JYFN1QZ9EY',
-  },
+  firebase,
   production: true,
   experimental: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,4 +4,5 @@ export const environment = {
   firebase,
   production: false,
   experimental: false,
+  useEmulators: true
 };
